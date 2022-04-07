fb5d7c19c912dbfcf1db728323a9d8c337a0f246
Sabuto <robe_dunne@hotmail.com>
2022-04-07 16:11:08 +0100

    Initial commit

---
+++ Cryptfest.Avalonia.Demo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cryptfest.Avalonia.Demo.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ Cryptfest.Avalonia.Demo/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Cryptfest.Avalonia.Demo.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
